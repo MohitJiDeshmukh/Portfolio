ffef4344ea783bf97a0b49831aa47ef09a395729
MohitJiDeshmukh <mohitdeshmukh203@gmail.com>
2024-10-03 04:54:22 +0530

    deploymentt commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { HashRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <Router>
     <App />
-  </HashRouter>
+  </Router>
 );
